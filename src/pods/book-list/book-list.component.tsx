a2385683544ebf2879d01e9bf70f3ff186348558
juanpms2 <juanpablo@lemoncode.net>
2023-10-24 12:45:52 +0200

    updated book list

--- src/pods/book-list/book-list.component.tsx
+++ src/pods/book-list/book-list.component.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
 import { Book } from './api';
 import * as classes from './book-list.styles';
 
 interface Props {
   bookList: Book[];
-  className?: string;
 }
 
 export const BookList: React.FC<Props> = props => {
-  const { bookList, className } = props;
+  const { bookList } = props;
 
   return (
-    <div className={className}>
-      <h1>Libros</h1>
-      <div className={classes.root}>
+    <div className={classes.root}>
+      <Typography className={classes.title} variant="h1" component={'h1'} aria-label="tienda online, listado de libros">
+        Libros
+      </Typography>
+      <div className={classes.cardContainer}>
         {bookList.map(book => (
-          <ul key={book.id} className={classes.container}>
-            <img src={book.image} alt={book.title} />
-            <h3>{book.title}</h3>
-          </ul>
+          // <ul key={book.id} className={classes.container}>
+          //   <img src={book.image} alt={book.title} />
+          //   <h3>{book.title}</h3>
+          // </ul>
+          <Card className={classes.card}>
+            <CardMedia image={book.image} title={`Portada del libro ${book.title}`} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {book.title}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Compartir</Button>
+              <Button size="small">Leer más</Button>
+            </CardActions>
+          </Card>
         ))}
       </div>
     </div>
--- src/pods/book-list/book-list.styles.ts
+++ src/pods/book-list/book-list.styles.ts
@@ -1,18 +1,55 @@
 import { css } from '@emotion/css';
+import { cardMediaClasses, cardActionsClasses } from '@mui/material';
 import { theme } from '@/core/theme';
 
 export const root = css`
-  display: grid;
-  @media (min-width: ${theme.breakpoints.values.md}px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media (min-width: ${theme.breakpoints.values.lg}px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-`;
-
-export const container = css`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 64px;
+  padding: ${theme.spacing(2)};
+  padding-bottom: ${theme.spacing(8)};
+  border: 1px solid ${theme.palette.primary.main};
+`;
+
+export const title = css`
+  align-self: flex-start;
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const cardContainer = css`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 64px;
+  justify-content: center;
+  max-width: 1280px;
+`;
+
+export const card = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  width: 350px;
+  height: 500px;
+  padding: ${theme.spacing(2)};
+
+  .${cardMediaClasses.root} {
+    height: 100%;
+    width: 100%;
+    border-radius: 8px;
+    background-position: top center;
+  }
+
+  .${cardActionsClasses.root} {
+    width: 100%;
+    justify-content: space-between;
+  }
+`;
+
+export const cardImage = css`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
