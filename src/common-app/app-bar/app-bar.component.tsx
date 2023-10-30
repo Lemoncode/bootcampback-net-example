57939dab0086e0b5af6d6d1c8936ca466f74d16d
juanpms2 <juanpablo@lemoncode.net>
2023-10-30 12:20:32 +0100

    updated logout

--- src/common-app/app-bar/app-bar.component.tsx
+++ src/common-app/app-bar/app-bar.component.tsx
@@ -4,15 +4,18 @@ import { AppBar, Toolbar, IconButton } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import UserIcon from '@mui/icons-material/Person';
 import LogoutIcon from '@mui/icons-material/ExitToApp';
+import { useAuthContext } from '@/core/auth';
 import { switchRoutes } from '@/core/router';
 import * as api from './api';
 import * as classes from './app-bar.styles';
 
 export const AppBarComponent: React.FC = () => {
   const navigate = useNavigate();
+  const { setIsUserLogged } = useAuthContext();
 
   const handleClick = async () => {
     await api.logout();
+    setIsUserLogged(false);
     navigate(switchRoutes.root);
   };
 
